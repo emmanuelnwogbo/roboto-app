430f7eeabefaf7f384dc1318a9e9d5644d992162
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-04-30 04:56:18 +0100

    added basic redux setup with redux-thunk and redux-logger for logging dispatch actions during production

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import Scroll from './components/Scroll';
 import ErrorBoundry from './components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: ''
-    };
-    this.onSearchChange = this.onSearchChange.bind(this)
+import { setSearchField, requestRobots } from './actions';
+
+const mapStateToProps = state => {
+  //the search field we'll return, which will be added to 
+  //the props of the
+  //app component
+  //console.log(state)
+  return {
+    searchField: state.searchRobots.searchField,
+    robots: state.requestRobots.robots,
+    isPending: state.requestRobots.isPending,
+    error: state.requestRobots.error
   }
+}
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+  //onRequestRobots: () => requestRobots(dispatch)
+  onRequestRobots: () => dispatch(requestRobots())
+})
+
+class App extends Component {
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response=> response.json())
-      .then(users => {this.setState({ robots: users})});
+    //console.log(this.props.store)
+    //console.log(this.props.store.getState())
+    //console.log(this.props)
+    this.props.onRequestRobots()
   }
 
-  onSearchChange (event) {
+  /*onSearchChange (event) {
     this.setState({ searchfield: event.target.value })
-  }
+  }*/
 
   render() {
-    const { robots, searchfield } = this.state;
+    const { searchField, onSearchChange, robots, isPending } = this.props;
     const filteredRobots = robots.filter(robot =>{
-      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    return !robots.length ?
+    return isPending ?
       <h1>Loading</h1> :
       (
         <div className='tc'>
           <h1 className='f1'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange}/>
+          <SearchBox searchChange={/*this.onSearchChange*/onSearchChange}/>
           <Scroll>
             <ErrorBoundry>
               <CardList robots={filteredRobots} />
@@ -46,4 +61,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(App);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import App from './App';
+import { searchRobots, requestRobots } from './reducers';
 
-ReactDOM.render(<App /> , document.getElementById('root'))
+//redux-logger is a middleware that 
+//console.logs the state changes in redux
+const logger = createLogger();
+
+const rootReducer = combineReducers({
+  searchRobots,
+  requestRobots
+});
+
+const store = createStore(
+  rootReducer, 
+  applyMiddleware(thunkMiddleware, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+)
